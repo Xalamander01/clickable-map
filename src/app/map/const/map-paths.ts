a4c755052177dc71fc6834e50376ab48434f993a
AlexStan <109579607+Xalamander01@users.noreply.github.com>
2025-07-14 11:16:32 +0300

    Adding EU map

--- src/app/map/const/map-paths.ts
+++ src/app/map/const/map-paths.ts
@@ -3,4 +3,5 @@ import { MapRegions } from './map-regions';
 export const MapPaths: Record<MapRegions, string> = {
     [MapRegions.US]: 'assets/us-map.svg',
     [MapRegions.BG]: 'assets/bg-map.svg',
+    [MapRegions.EU]: 'assets/eu-map.svg'
 };
